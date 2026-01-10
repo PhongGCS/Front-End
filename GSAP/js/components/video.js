48450a28accfc3bfc93d06925012a2c54d0b79f4
loc-tong-covergo <loc.tong@covergo.com>
2026-01-10 17:40:11 +0700

    fix video + display content

--- GSAP/js/components/video.js
+++ GSAP/js/components/video.js
@@ -3,9 +3,13 @@ import Component from "./index.js";
 export class Video extends Component {
   video;
   videoSrc;
+  contents;
+  contentSegments = [];
   duration = 0;
   timeline;
   objectUrl;
+  activated = false;
+  readyMarked = false;
 
   constructor(el) {
     super(el);
@@ -15,6 +19,7 @@ export class Video extends Component {
     if (!this.video) return;
 
     this.videoSrc = this.video.getAttribute("src");
+    this.contents = this.refs.content;
   }
 
   async setup() {
@@ -80,14 +85,21 @@ export class Video extends Component {
     this.video.currentTime = 0;
     this.video.muted = true;
     this.video.playsInline = true;
+    this.video.setAttribute("playsinline", "");
+    this.video.setAttribute("webkit-playsinline", "");
+    this.video.disableRemotePlayback = true;
 
     // iOS activation to allow programmatic seeking.
     this.once(document.documentElement, "touchstart", () => {
-      this.video.play();
-      this.video.pause();
+      this.tryActivate();
+    });
+    this.once(document.documentElement, "pointerdown", () => {
+      this.tryActivate();
     });
 
     this.createScrollScrub();
+    this.prepareContentSegments();
+    this.markReady();
   }
 
   createScrollScrub() {
@@ -105,9 +117,72 @@ export class Video extends Component {
       scrub: 1,
       pin: true,
       animation: this.timeline,
-      onEnter: () => this.video.pause(),
-      onEnterBack: () => this.video.pause(),
+      onEnter: () => {
+        this.tryActivate();
+        this.video.pause();
+      },
+      onEnterBack: () => {
+        this.tryActivate();
+        this.video.pause();
+      },
       onRefresh: () => this.video.pause(),
+      onUpdate: (self) => this.syncContent(self.progress),
     });
+  }
+
+  prepareContentSegments() {
+    const items = Array.isArray(this.contents)
+      ? this.contents
+      : [this.contents].filter(Boolean);
+    if (!items.length) return;
+    const segmentCount = items.length;
+    this.contentSegments = [];
+    for (let i = 0; i < segmentCount; i++) {
+      this.contentSegments.push({
+        el: items[i],
+        start: i / segmentCount,
+        end: (i + 1) / segmentCount,
+      });
+      // hide all initially
+      gsap.set(items[i], { opacity: 0, pointerEvents: "none" });
+    }
+  }
+
+  syncContent(progress) {
+    if (!this.contentSegments.length) return;
+    // find current segment by progress
+    const active = this.contentSegments.find(
+      (seg) => progress >= seg.start && progress < seg.end
+    );
+    this.contentSegments.forEach((seg) => {
+      const visible = seg === active;
+      gsap.to(seg.el, {
+        opacity: visible ? 1 : 0,
+        duration: visible ? 0.3 : 0.2,
+        ease: "power1.out",
+        overwrite: true,
+      });
+      gsap.set(seg.el, { pointerEvents: visible ? "auto" : "none" });
+    });
+  }
+
+  tryActivate() {
+    if (this.activated) return;
+    this.activated = true;
+    // Play/pause to unlock currentTime updates on iOS.
+    const p = this.video.play();
+    if (p && typeof p.then === "function") {
+      p.then(() => this.video.pause()).catch(() => {});
+    } else {
+      this.video.pause();
+    }
+  }
+
+  markReady() {
+    if (this.readyMarked) return;
+    this.readyMarked = true;
+    document.documentElement.classList.remove("is-loading");
+    const loader = document.getElementById("page-loader");
+    if (loader) loader.style.display = "none";
   }
 }
